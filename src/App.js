bf8e27528b41e1fe83aac7318c7da320a94a9636
xannr1 <shanewalworth@gmail.com>
2022-09-18 14:05:35 -0400

    minor changes

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import './App.css';
 
 function App() {
   return (
-
-    <div>
- <h2>Github Pages</h2>
- <h3>Deploying React to Github</h3>
- </div>
-
-
-
+    // <div>
+    // <h2>Github Pages</h2>
+    // <h3>Deploying React to Github</h3>
+    // </div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
   );
 }
 
